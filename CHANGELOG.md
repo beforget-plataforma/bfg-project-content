f08429e4a732ff2ca3775423f1bb0fb67a360b6e
sonidiaz <nachodiaz8@gmail.com>
2021-05-26 12:16:56 +0200

    Fix filter profile list project

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,24 @@
+##  2.1 (mayo, 2021)
+###  Corrección de listado en perfiles
+* El funcionamiento del filtro por perfil del usuario no era el correcto. Los resultados que se obtenían en algunos casos eran correctos pero en otros no.
+El retorno del valor del miembro era un Objecto.
+
+```
+//$profileUserID es el ID del perfil que se está visitando.
+
+$args = array(
+	'post_type' => 'proyectos',
+	'meta_query' => array(
+		array(
+			'key' => 'miembros',
+			'value' => $profileUserID,
+			'compare' => 'REGEXP'
+		)
+	)
+);
+
+```
+
 ## 2.0 (Mayo, 2021)
 
 ### Filtrado de proyectos
--- includes/bfg-project-display-profile.php
+++ includes/bfg-project-display-profile.php
@@ -12,18 +12,15 @@ function bfg_proyectos_posts() {
 
 	$authorId = get_the_author_meta( 'ID' );
 	$profileUserID = bp_displayed_user_id();
-	$current_user = wp_get_current_user();
+	$current_user = wp_get_current_user();	
+	$existMember = false;
+
 	
 	$userData = get_userdata($user_id);
 	$args = array(
 		'post_type' => 'proyectos',
-		'meta_query' => array(
-			array(
-				'key' => 'miembros',
-				'value' => $profileUserID,
-				'compare' => 'REGEXP'
-			)
-		)
+		'posts_per_page' => -1,
+		'orderbyby' => 'DESC',
 	);
 	$the_query = new WP_Query($args);
 	if ($the_query->have_posts()) {
@@ -38,66 +35,73 @@ function bfg_proyectos_posts() {
 					$thumbID = get_post_thumbnail_id( $post->ID );
 					$imgDestacada = wp_get_attachment_url( $thumbID );
 					$miembros = get_post_meta( get_the_ID(), 'miembros', true);
-				?>
-				<? if(false !== array_search($profileUserID, $miembros)): ?>
-					<div class="bfg-item-proyectos">
-						<a class="no-color" href="<?php the_permalink(); ?>">
-							<div class="bfg-header-cover-sesiones bfg-has-avatar item-profile flex" style="background-image:url(<?php echo $imgDestacada; ?>)">
-							</div>
-							<div class="bfg-avatar-proyecto" style="background-image: url(<? echo the_field('logo_proyecto'); ?>)">
-							</div>
-							<hgroup class="bfg-content-inprofile resumen-proyecto">
-								<div class=" ">
-									<?php
-										$title = get_the_title();
-										$short_title = wp_trim_words( $title, 12, '...' );
-									?>
-									<h2 class="title-bit"><? echo $short_title; ?></h2>
-								</div>
-								<div class="group-description">
-									<? echo the_excerpt(); ?>
-								</div>
-							</hgroup>
-							<div class="bfg-miembros-proyecto flex bfg-flex-grap">
-									<?
-									// $user_id = get_the_author_meta( 'ID' );
-									foreach($miembros as $userID){
-										$userName = xprofile_get_field_data('1', $userID);
-										$args = array( 
-											'item_id' => $userID
-										); 
-										echo bp_core_fetch_avatar($args); 
-									}
-								?>
-							</div>
-							<div class="bfg-ods-proyecto">
-								<ul class="bfg-list flex">
-									<?if ($terms):?>
-										<?php foreach($terms as $term): ?>
-											<li class="<?php echo $term->slug; ?>">
-												<img src="<? echo get_stylesheet_directory_uri() . '/assets/images/' . $term->slug . '.png';  ?>" alt="">
-											</li>
-										<?php endforeach; ?>
-									<?php endif;?>
-								</ul>
-							</div>
-						</a>
-					</div>
-				<? else: ?>
-					<?
-						$count ++;
-						if(($count + 1) == $the_query->found_posts){
-							?>
-								<aside class="bp-feedback bp-messages info">
-										<span class="bp-icon" aria-hidden="true"></span>
-										<p>
-											Aún no tienes proyectos.
-										</p>
-								</aside>
-									<?
-							}
+
+					?>
+				<? if($miembros): ?>
+					<? if(false !== array_search($profileUserID, $miembros)): 
 						?>
+						<? $existMember = true; ?>
+
+						<div class="bfg-item-proyectos">
+							<a class="no-color" href="<?php the_permalink(); ?>">
+								<div class="bfg-header-cover-sesiones bfg-has-avatar item-profile flex" style="background-image:url(<?php echo $imgDestacada; ?>)">
+								</div>
+								<div class="bfg-avatar-proyecto" style="background-image: url(<? echo the_field('logo_proyecto'); ?>)">
+								</div>
+								<hgroup class="bfg-content-inprofile resumen-proyecto">
+									<div class=" ">
+										<?php
+											$title = get_the_title();
+											$short_title = wp_trim_words( $title, 12, '...' );
+										?>
+										<h2 class="title-bit"><? echo $short_title; ?></h2>
+									</div>
+									<div class="group-description">
+										<? echo the_excerpt(); ?>
+									</div>
+								</hgroup>
+								<div class="bfg-miembros-proyecto flex bfg-flex-grap">
+										<?
+										// $user_id = get_the_author_meta( 'ID' );
+										foreach($miembros as $userID){
+											$userName = xprofile_get_field_data('1', $userID);
+											$args = array( 
+												'item_id' => $userID
+											); 
+											echo bp_core_fetch_avatar($args); 
+										}
+									?>
+								</div>
+								<div class="bfg-ods-proyecto">
+									<ul class="bfg-list flex">
+										<?if ($terms):?>
+											<?php foreach($terms as $term): ?>
+												<li class="<?php echo $term->slug; ?>">
+													<img src="<? echo get_stylesheet_directory_uri() . '/assets/images/' . $term->slug . '.png';  ?>" alt="">
+												</li>
+											<?php endforeach; ?>
+										<?php endif;?>
+									</ul>
+								</div>
+							</a>
+						</div>
+					<? else: ?>
 				<? endif; ?>
+						
+				<? endif; ?>
+				<?
+				if((($count + 1) == $the_query->found_posts) && !$existMember){
+					?>
+						<aside class="bp-feedback bp-messages info">
+								<span class="bp-icon" aria-hidden="true"></span>
+								<p>
+									<? echo xprofile_get_field_data('1', $profileUserID)?> aún no tienes proyectos.
+								</p>
+						</aside>
+							<?
+					}
+					$count ++;
+					?>
 			<?
 		}
 		?>
